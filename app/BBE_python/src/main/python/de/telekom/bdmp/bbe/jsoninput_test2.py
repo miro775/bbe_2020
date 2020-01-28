146028cb15326ce21d4ac54c7a14174f4d4518be
Miroslav Just <miroslav.just@t-mobile.cz>
2020-01-28 18:29:59 +0100

    filter-test

--- app/BBE_python/src/main/python/de/telekom/bdmp/bbe/jsoninput_test2.py
+++ app/BBE_python/src/main/python/de/telekom/bdmp/bbe/jsoninput_test2.py
@@ -12,6 +12,10 @@ spark = SparkSession \
     .config("spark.executor.instances", "2") \
     .getOrCreate()
 
-df1 = spark.sql("select * from db_d170_bbe_iws_pwr.IL_TMagic_jsoninput_ET").filter('messagetype = ''DigiOSS - FibreOnLocation''')
-df1.filter('acl_id = 100053607')
-df1.show()
+df1 = spark.sql("select * from db_d170_bbe_iws_pwr.IL_TMagic_jsoninput_ET")
+
+df2 = df1.filter((df1['messagetype'] == 'DigiOSS - FibreOnLocation') & (df1['acl_id'] == '100053607'))  \
+    .select('jsonstruct').collect()
+
+print(df2)
+
