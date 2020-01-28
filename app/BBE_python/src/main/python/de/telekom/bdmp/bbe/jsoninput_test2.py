c1c2769e659c36d655669850334234faee8cf2b5
Miroslav Just <miroslav.just@t-mobile.cz>
2020-01-28 20:31:41 +0100

    filter-test json parsing

--- app/BBE_python/src/main/python/de/telekom/bdmp/bbe/jsoninput_test2.py
+++ app/BBE_python/src/main/python/de/telekom/bdmp/bbe/jsoninput_test2.py
@@ -1,5 +1,7 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json
+from pyspark.sql.functions import col
 from datetime import datetime
 
 ts = datetime.now().strftime('%Y%m%d%H%M')
@@ -14,8 +16,20 @@ spark = SparkSession \
 
 df1 = spark.sql("select * from db_d170_bbe_iws_pwr.IL_TMagic_jsoninput_ET")
 
-df2 = df1.filter((df1['messagetype'] == 'DigiOSS - FibreOnLocation') & (df1['acl_id'] == '100053607'))  \
-    .select('jsonstruct').collect()
+#df2 = df1.filter((df1['messagetype'] == 'DigiOSS - FibreOnLocation') & (df1['acl_id'] == '100053607'))  \
+#    .select('jsonstruct').collect()
+#print(df2)
 
-print(df2)
+df3 = df1.filter((df1['messagetype'] == 'DigiOSS - FibreOnLocation') & (df1['acl_id'] == '100053607'))
+df3.show()
+
+#df_jsonschema = spark.read.json(df3.rdd.map(lambda row: row.jsonstruct))
+#df_jsonschema.printSchema()
+
+jsonschema0 = spark.read.json(df3.rdd.map(lambda row: row.jsonstruct)).schema
+
+df4 = df3.withColumn('jsonstruct', from_json(col('jsonstruct'), jsonschema0))
+df4.show()
+
+
 
