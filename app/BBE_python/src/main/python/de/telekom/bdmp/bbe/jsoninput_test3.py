8a1ab85839fe88b34551dadb5a6518866964b8ff
Miroslav Just <miroslav.just@t-mobile.cz>
2020-01-31 13:48:25 +0100

    filter-test3 json parsing,  miro

---
+++ app/BBE_python/src/main/python/de/telekom/bdmp/bbe/jsoninput_test3.py
@@ -0,0 +1,44 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json
+from pyspark.sql.functions import col
+from datetime import datetime
+
+ts = datetime.now().strftime('%Y%m%d%H%M')
+
+#coment1
+
+spark = SparkSession \
+    .builder \
+    .appName("miro_bbe_session_{ts}") \
+    .config("spark.executor.memory", "2g") \
+    .config("spark.executor.cores", "1") \
+    .config("spark.executor.instances", "2") \
+    .getOrCreate()
+
+df1 = spark.sql("select * from db_d170_bbe_iws_pwr.IL_TMagic_jsoninput_ET")
+
+#df2 = df1.filter((df1['messagetype'] == 'DigiOSS - FibreOnLocation') & (df1['acl_id'] == '100053607'))  \
+#    .select('jsonstruct').collect()
+#print(df2)
+
+# filter , 1 record
+df3 = df1.filter((df1['messagetype'] == 'DigiOSS - FibreOnLocation') & (df1['acl_id'] == '100053607'))
+df3.show()
+
+# read column 'jsonstruct' as JSON
+#df_jsonschema = spark.read.json(df3.rdd.map(lambda row: row.jsonstruct))
+#df_jsonschema.printSchema()
+
+#get schema from json column 'jsonstruct'
+jsonschema_FoL = spark.read.json(df3.rdd.map(lambda row: row.jsonstruct)).schema
+
+df4 = df3.withColumn('jsonstruct', from_json(col('jsonstruct'), jsonschema_FoL)).select('jsonstruct.*')
+df4.show()
+
+
+
+df_FoL_miro = spark.table('db_d170_bbe_in_iws.il_tmagic_fiberOnLocation_miro1_mt')
+df_FoL_miro.show()
+
+
