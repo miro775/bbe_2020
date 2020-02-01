92365f974f6806bfb6fc8cbfa8510907ff779e61
Miroslav Just <miroslav.just@t-mobile.cz>
2020-02-01 20:03:01 +0100

    filter-test4 json parsing, insert DF

--- app/BBE_python/src/main/python/de/telekom/bdmp/bbe/jsoninput_test4.py
+++ app/BBE_python/src/main/python/de/telekom/bdmp/bbe/jsoninput_test4.py
@@ -39,61 +39,30 @@ jsonschema_FoL = spark.read.json(df3.rdd.map(lambda row: row.jsonstruct)).schema
 #Returns a new DataFrame by adding a column or replacing the existing column that has the same name.
 #The column expression must be an expression over this DataFrame; attempting to add a column from some other dataframe will raise an error.
 
-df4jsn = df3.withColumn('json_data', from_json(col('jsonstruct'), jsonschema_FoL))
+#df4jsn = df3.withColumn('json_data', from_json(col('jsonstruct'), jsonschema_FoL))
 
-# show data from json-struct
-#df4jsn = df3.withColumn('jsonstruct2', from_json(col('jsonstruct'), jsonschema_FoL)).select('jsonstruct.*')
+
+df4jsn = df3.withColumn('json_data', from_json(col('jsonstruct'), jsonschema_FoL))\
+    .select(
+    col('acl_id'),
+	col('acl_dop'),
+	col('acl_dop_yyyymm'),
+    col('acl_loadnumber'),
+	col('messagetype'),
+	col('messageversion'),
+    col('json_data.creationDate'),
+    col('json_data.modificationDate'),
+    col('json_data.klsId'),
+    col('json_data.areaType')
+)
+    # do not use show: .show()
+
+#insert dataframe into table
+df4jsn.write.insertInto( 'db_d170_bbe_in_iws.il_tmagic_fiberOnLocation_demo0_mt', overwrite=False)
 #df4jsn.show()
 
 
-# test, show table
-#df_FoL_miro = spark.table('db_d170_bbe_in_iws.il_tmagic_fiberOnLocation_miro1_mt')
-#df_FoL_miro.show()
+# test, read and show data from table
+df_FoL_demo0 = spark.table('db_d170_bbe_in_iws.il_tmagic_fiberOnLocation_demo0_mt')
+df_FoL_demo0.show()
 
-
-#PipelinedRDD
-df_map1 = df4jsn.rdd.map(lambda p: (
-    p['acl_id'], p['acl_dop'], p['acl_dop_yyyymm'],
-    p['acl_loadnumber'], p['messagetype'], p['messageversion'],
-    p['json_data.creationDate'],
-    p['json_data.modificationDate'],
-    p['json_data.klsId'],
-    p['json_data.areaType']
-
-))
-
-
-schema1_FoL = StructType([StructField('acl_id_int', StringType()),
-                          StructField('acl_dop_iso', StringType()),
-                          StructField('acl_dop_yyyymm', StringType()),
-                          StructField('acl_loadnumber', StringType()),
-                          StructField('messagetype', StringType()),
-                          StructField('messageversion', StringType()),
-                          StructField('creationDate', LongType()),
-                          StructField('modificationDate', LongType()),
-                          StructField('klsId', StringType()),
-                          StructField('areaType', StringType())
-                         ])
-
-
-
-df_FoL_table = spark.createDataFrame(df_map1,  schema1_FoL)
-
-
-'''
-df_map2 = df4jsn.rdd.map(lambda p: (
-    p['creationDate'], p['modificationDate'], p['klsId'], p['areaType']))
-
-schema2_FoL = StructType([StructField('creationDate', IntegerType()),
-                          StructField('modificationDate', IntegerType()),
-                          StructField('klsId', StringType()),
-                          StructField('areaType', StringType())
-                         ])
-'''
-
-
-
-
-#insert dataframe to table
-df_FoL_table.write.insertInto( 'db_d170_bbe_in_iws.il_tmagic_fiberOnLocation_demo0_mt', overwrite=False)
-df_FoL_table.show()
