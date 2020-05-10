5bf63b0f0deb0721a1e24faae6349fe4e9f86805
Miroslav Just <miroslav.just@t-mobile.cz>
2020-05-10 18:37:04 +0200

    PSO-orderItem, test-parsing  [json in array], post-ex from json-column:  orderitems_json, in progress

--- app/BBE_python/src/main/python/de/telekom/bdmp/bbe/demo1/pso_orderitem_test.py
+++ app/BBE_python/src/main/python/de/telekom/bdmp/bbe/demo1/pso_orderitem_test.py
@@ -7,7 +7,7 @@ import pyspark.sql.functions as F
 
 from datetime import datetime
 
-input_gigabit_table = 'db_d172_bbe_core_iws.cl_f_presalesorder_mt'
+input_gigabit_table = 'db_d174_bbe_core_iws.cl_f_presalesorder_mt'
 #--   db_d172_bbe_core_iws.cl_f_presalesorder_mt_orderItem_test;
 
 
@@ -26,22 +26,42 @@ df_input = spark0.sql("select * from  {0}".format(input_gigabit_table))
 df_al = df_input.filter((df_input['acl_id_int'] == 200655) | (df_input['acl_id_int'] == 200742))
 
 json_schema_reduced = spark0.read.json(
-    df_al.rdd.map(lambda row: row.orderitems_struct))
+    df_al.rdd.map(lambda row: row.orderitems_json))
 json_schema_reduced.printSchema()  # debug only
 
 
-#  select  acl_id_int,orderitems_struct,  bdmp_area_id from  db_d172_bbe_core_iws.cl_f_presalesorder_mt where acl_id_int in ( 200655, 200742) ;
 
-df_al = df_al.select(df_al['acl_id_int'],df_al['orderitems_struct'])
+# select  acl_id_int,orderitems_array,orderitems_json
+# from  db_d174_bbe_core_iws.cl_f_presalesorder_mt where acl_id_int in ( 200655, 200742) ;
+
+df_al = df_al.select(df_al['acl_id_int'],df_al['orderitems_json'])
 df_al.show(2,True)
 
 '''
 
+# acl_id=200742 has only 1 orderItem in array:   
+   MagentaZuhause XXL mit MagentaTV	= service 
+ 
+# acl_id=200655 has 4 orderItems in array:
+    MagentaZuhause Giga = service
+    Media Receiver 401 schwarz = receiver
+    Speedport Smart 3 = router
+    Glasfaser-Modem	= ont
+
 +----------+--------------------+
-|acl_id_int|   orderitems_struct|
+|acl_id_int|    orderitems_array|
 +----------+--------------------+
-|    200742|[{"id":"PoH{S","o...|
+|    200655|[[[[ZK01, 633,, 3...|
+|    200742|[[[[ZK01, 1492, M...|
++----------+--------------------+
+
+! orderitems_json = [{json in array}]
+
++----------+--------------------+
+|acl_id_int|     orderitems_json|
++----------+--------------------+
 |    200655|[{"id":"Pg3äS","o...|
+|    200742|[{"id":"PoH{S","o...|
 +----------+--------------------+
 
 '''
@@ -49,26 +69,49 @@ df_al.show(2,True)
 #  pyspark.sql.utils.AnalysisException: "cannot resolve 'explode(cl_f_presalesorder_mt.`orderitems_struct`)'
 #  due to data type mismatch: input to function explode should be array or map type, not string;
 
-# which schema?   json_schema_reduced.schema /  json_schema_full.schema
-df_al_json2 = df_al.withColumn('json_data', F.from_json(F.col('orderitems_struct'), json_schema_reduced.schema)) \
+df_al_json2 = df_al.withColumn('json_data', F.from_json(F.col('orderitems_json'), json_schema_reduced.schema)) \
     .select("json_data.*")
 df_al_json2.show(2, False)
 
 '''
-        # ciastocny uspech, ked je iba jeden [] v array,tak to funguje pre ten jeden,,,,: inak asi nejak pouzit explode() ?
-        select("json_data.*")
+# ciastocny uspech, ked je iba jeden [] v array,tak to funguje pre ten jeden,,,,: inak asi nejak pouzit explode() ?
+ select("json_data.*")
         
+
 +----------------------------------------------------------------------------------------------------------------------------------------+-----+--------+--------------------------------+-------------------+-----------------+-----------+-----------------+
 |conditions                                                                                                                              |id   |itemType|name                            |orderId            |performProcessing|pricingDate|productMaterialId|
 +----------------------------------------------------------------------------------------------------------------------------------------+-----+--------+--------------------------------+-------------------+-----------------+-----------+-----------------+
-|[[ZK01, 1492, MagentaZuhause XXL mit, 730, true, 69.95, 1001328737], [ZK02, 1493, MagentaZuhause XXL mit, 730, true, 79.95, 1001363672]]|PoH{S|service |MagentaZuhause XXL mit MagentaTV|P&${WjZqiN`L6SjljmS|true             |2020-03-17 |89987868         |
 |null                                                                                                                                    |null |null    |null                            |null               |null             |null       |null             |
+|[[ZK01, 1492, MagentaZuhause XXL mit, 730, true, 69.95, 1001328737], [ZK02, 1493, MagentaZuhause XXL mit, 730, true, 79.95, 1001363672]]|PoH{S|service |MagentaZuhause XXL mit MagentaTV|P&${WjZqiN`L6SjljmS|true             |2020-03-17 |89987868         |
 +----------------------------------------------------------------------------------------------------------------------------------------+-----+--------+--------------------------------+-------------------+-----------------+-----------+-----------------+
 
 
 '''
 
-#df_al_json2.printSchema()
+df_al_json2.printSchema()
+
+'''
+
+root
+ |-- conditions: array (nullable = true)
+ |    |-- element: struct (containsNull = true)
+ |    |    |-- conditionType: string (nullable = true)
+ |    |    |-- id: long (nullable = true)
+ |    |    |-- invoiceText: string (nullable = true)
+ |    |    |-- orderItemId: long (nullable = true)
+ |    |    |-- performProcessing: boolean (nullable = true)
+ |    |    |-- price: string (nullable = true)
+ |    |    |-- telekomConditionId: string (nullable = true)
+ |-- id: string (nullable = true)
+ |-- itemType: string (nullable = true)
+ |-- name: string (nullable = true)
+ |-- orderId: string (nullable = true)
+ |-- performProcessing: boolean (nullable = true)
+ |-- pricingDate: string (nullable = true)
+ |-- productMaterialId: string (nullable = true)
+
+
+'''
 
 
 
